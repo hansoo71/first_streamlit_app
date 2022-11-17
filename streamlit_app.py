28a81facff79e8b64713a6803a753e754d0ab4bb
Hansoo KIm <hansoo71@gmail.com>
2022-11-17 09:55:24 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('reakfast Favorites')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
